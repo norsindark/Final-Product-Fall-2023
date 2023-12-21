580f62f52d86778d33bf1bb848bd93281e728b74
SinD <107561293+norsindark@users.noreply.github.com>
2023-12-21 08:39:05 +0700

    Update network.js

--- client/src/constant/network.js
+++ client/src/constant/network.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://localhost:9999";
-const WS_URL = "https://localhost:9999/chair-booking"
+const BASE_URL = "http://localhost:9999";
+const WS_URL = "http://localhost:9999/chair-booking"
 
 export { BASE_URL, WS_URL }
